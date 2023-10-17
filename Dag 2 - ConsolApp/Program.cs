14469e9ef8e80eb6b084b0c2211ec14f582c0727
jonashojer <86884161+jonashojer@users.noreply.github.com>
2023-10-17 12:29:36 +0200

    oprettelse af arrays

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -1,35 +1,17 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+﻿/*string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";*/
+
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
 
 
-// Igen vil det være smartere at bruge en switch
-/*switch (daysUntilExpiration)
-{
-	case 0:
-		Console.WriteLine("Your subscription has expired.\n");
-		break;
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 
-	case 1:
-		Console.WriteLine("Your subscription expires within a day!\nRenew now and save 20%!");
-		break;
+fraudulentOrderIDs[0] = "F000";
 
-	case <= 5:
-		Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10 % !");
-		break;
-
-	case <= 10:
-		Console.WriteLine("Your subscription will expire soon. Renew now!\n");
-		break;
-}*/
-
-Console.WriteLine(daysUntilExpiration);
-if (daysUntilExpiration == 0) {
-	Console.WriteLine("Your subscription has expired.\n");
-} else if (daysUntilExpiration == 1) {
-	Console.WriteLine("Your subscription expires within a day!\nRenew now and save 20%!");
-} else if (daysUntilExpiration <= 5) {
-	Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10 % !");
-} else if (daysUntilExpiration <= 10) {
-	Console.WriteLine("Your subscription will expire soon. Renew now!\n");
-} 
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
