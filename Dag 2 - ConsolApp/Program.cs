5ff3587eaf52c39f9a314b06b1d73490b47f8e9c
jonashojer <86884161+jonashojer@users.noreply.github.com>
2023-10-17 12:19:51 +0200

    if statement med subcription

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -1,60 +1,35 @@
-﻿Random dice = new Random();
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
 
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// Igen vil det være smartere at bruge en switch
+/*switch (daysUntilExpiration)
 {
-	if ((roll1 == roll2) && (roll2 == roll3))
-	{
-		Console.WriteLine("You rolled doubles! +6 bonus to total!");
-		total += 6;
-	}
-	else
-	{
-		Console.WriteLine("You rolled doubles! +2 bonus to total!");
-		total += 2;
-	}
-}
-
-
-// Vil være smartere med en switch her
-/*switch (total){
-	case >= 16:
-		Console.WriteLine("You win a new car!");
+	case 0:
+		Console.WriteLine("Your subscription has expired.\n");
 		break;
 
-	case >= 10:
-		Console.WriteLine("You win a new laptop!");
+	case 1:
+		Console.WriteLine("Your subscription expires within a day!\nRenew now and save 20%!");
 		break;
 
-	case 7:
-		Console.WriteLine("You win a trip for two!");
+	case <= 5:
+		Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10 % !");
 		break;
 
-	default:
-		Console.WriteLine("You win a kitten!");
+	case <= 10:
+		Console.WriteLine("Your subscription will expire soon. Renew now!\n");
 		break;
 }*/
 
-if (total >= 16)
-{
-	Console.WriteLine("You win a new car!");
-}
-else if (total >= 10)
-{
-	Console.WriteLine("You win a new laptop!");
-}
-else if (total == 7)
-{
-	Console.WriteLine("You win a trip for two!");
-}
-else
-{
-	Console.WriteLine("You win a kitten!");
-}
+Console.WriteLine(daysUntilExpiration);
+if (daysUntilExpiration == 0) {
+	Console.WriteLine("Your subscription has expired.\n");
+} else if (daysUntilExpiration == 1) {
+	Console.WriteLine("Your subscription expires within a day!\nRenew now and save 20%!");
+} else if (daysUntilExpiration <= 5) {
+	Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10 % !");
+} else if (daysUntilExpiration <= 10) {
+	Console.WriteLine("Your subscription will expire soon. Renew now!\n");
+} 
