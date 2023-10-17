a66b7f972fb626279a63621610f1b067c7de4dcf
jonashojer <86884161+jonashojer@users.noreply.github.com>
2023-10-17 13:34:15 +0200

    code readability part 2

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -1,19 +1,17 @@
-﻿Random dice = new Random();
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-int total = roll1 + roll2 + roll3;
+﻿// convert the message into a char array
+string str = "The quick brown fox jumps over the lazy dog.";
+char[] charMessage = str.ToCharArray();
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// Reverse the chars
+Array.Reverse(charMessage);
 
+// count the o's
+int x = 0;
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
 
-// Jeg synes det er meget mere readable med { på samme ligne som statement - det er nok bare fordi det er sådan at jeg har lært det.
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
-	if ((roll1 == roll2) && (roll2 == roll3)) {
-		Console.WriteLine("You rolled triples!  +6 bonus to total!");
-		total += 6;
-	} else {
-		Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-		total += 2;
-	}
-}
+// convert it back to a string
+string new_message = new String(charMessage);
+
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
