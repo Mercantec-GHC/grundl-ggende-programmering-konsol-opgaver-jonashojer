846e231275533a9a7885da377f7c6423d6594bc0
jonashojer <86884161+jonashojer@users.noreply.github.com>
2023-10-18 10:25:45 +0200

    some tasks from 3.1

--- Dag 3 - ConsolApp/Program.cs
+++ Dag 3 - ConsolApp/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("a" == "a");
+//Console.WriteLine("a" == "A");
+//Console.WriteLine(1 == 2);
+
+//string myValue = "a";
+//Console.WriteLine(myValue == "a");
+
+//Console.WriteLine("a" != "a");
+//Console.WriteLine("a" != "A");
+//Console.WriteLine(1 != 2);
+
+//string myValue = "a";
+//Console.WriteLine(myValue != "a");
+
+//Console.WriteLine(1 > 2);
+//Console.WriteLine(1 < 2);
+//Console.WriteLine(1 >= 1);
+//Console.WriteLine(1 <= 1);
+
+//string pangram = "The quick brown fox jumps over the lazy dog.";
+//Console.WriteLine(pangram.Contains("fox"));
+//Console.WriteLine(pangram.Contains("cow"));
+
+//Console.WriteLine(pangram.Contains("fox") == false);
+//Console.WriteLine(!pangram.Contains("fox"));
+
+//string pangram = "The quick brown fox jumps over the lazy dog.";
+//Console.WriteLine(!pangram.Contains("fox"));
+//Console.WriteLine(!pangram.Contains("cow"));
+
+//int saleAmount = 1001;
+//int discount = saleAmount > 1000 ? 100 : 50;
+//Console.WriteLine($"Discount: {discount}");
+
+//int saleAmount = 1001;
+
+//Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+Random random = new Random();
+
+int dice = random.Next(1, 21);
+
+string result = dice > 10 ? "Heads" : "Tails";
+
+Console.WriteLine(result);
+
