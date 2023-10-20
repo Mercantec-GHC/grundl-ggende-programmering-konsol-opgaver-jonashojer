c29e0a05df197bf1ae36eef9282e591e69148cbf
jonashojer <86884161+jonashojer@users.noreply.github.com>
2023-10-20 15:55:39 +0200

    done med mere i 4.1

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -1,22 +1,132 @@
-﻿////Console.WriteLine("Signed integral types:");
+﻿////////////////////////////////Console.WriteLine("Signed integral types:");
 
-////Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-////Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-////Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-////Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+////////////////////////////////Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+////////////////////////////////Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+////////////////////////////////Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+////////////////////////////////Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
 
-////Console.WriteLine("");
-////Console.WriteLine("Unsigned integral types:");
+////////////////////////////////Console.WriteLine("");
+////////////////////////////////Console.WriteLine("Unsigned integral types:");
 
-////Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-////Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-////Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-////Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+////////////////////////////////Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+////////////////////////////////Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+////////////////////////////////Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+////////////////////////////////Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
 
-//Console.WriteLine("");
-//Console.WriteLine("Floating point types:");
-//Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-//Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-//Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+//////////////////////////////Console.WriteLine("");
+//////////////////////////////Console.WriteLine("Floating point types:");
+//////////////////////////////Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+//////////////////////////////Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+//////////////////////////////Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
 
-int[] data = new int[3];
+////////////////////////////int[] data = new int[3];
+
+//////////////////////////int first = 2;
+//////////////////////////string second = "4";
+//////////////////////////string result = first + second;
+//////////////////////////Console.WriteLine(result);
+
+////////////////////////int myInt = 3;
+////////////////////////Console.WriteLine($"int: {myInt}");
+
+////////////////////////decimal myDecimal = myInt;
+////////////////////////Console.WriteLine($"decimal: {myDecimal}");
+
+//////////////////////decimal myDecimal = 3.14m;
+//////////////////////Console.WriteLine($"decimal: {myDecimal}");
+
+//////////////////////int myInt = (int)myDecimal;
+//////////////////////Console.WriteLine($"int: {myInt}");
+
+////////////////////decimal myDecimal = 1.23456789m;
+////////////////////float myFloat = (float)myDecimal;
+
+////////////////////Console.WriteLine($"Decimal: {myDecimal}");
+////////////////////Console.WriteLine($"Float  : {myFloat}");
+
+//////////////////int first = 5;
+//////////////////int second = 7;
+//////////////////string message = first.ToString() + second.ToString();
+//////////////////Console.WriteLine(message);
+
+////////////////string first = "5";
+////////////////string second = "7";
+////////////////int sum = int.Parse(first) + int.Parse(second);
+////////////////Console.WriteLine(sum);
+//////////////string value1 = "5";
+//////////////string value2 = "7";
+//////////////int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+//////////////Console.WriteLine(result);
+
+////////////int value = (int)1.5m; // casting truncates
+////////////Console.WriteLine(value);
+
+////////////int value2 = Convert.ToInt32(1.5m); // converting rounds up
+////////////Console.WriteLine(value2);
+
+//////////string name = "Bob";
+//////////Console.WriteLine(int.Parse(name));
+
+////////string value = "102";
+////////int result = 0;
+////////if (int.TryParse(value, out result))
+////////{
+////////	Console.WriteLine($"Measurement: {result}");
+////////}
+////////else
+////////{
+////////	Console.WriteLine("Unable to report the measurement.");
+////////}
+
+//////string value = "bad";
+//////int result = 0;
+//////if (int.TryParse(value, out result))
+//////{
+//////	Console.WriteLine($"Measurement: {result}");
+//////}
+//////else
+//////{
+//////	Console.WriteLine("Unable to report the measurement.");
+//////}
+
+//////if (result > 0)
+//////	Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+////string hest = "2.71828";
+////decimal.TryParse(hest, out decimal myHest);
+
+//string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+//decimal total = 0m;
+//string message = "";
+
+//foreach (var value in values)
+//{
+//	decimal number; // stores the TryParse "out" value
+//	if (decimal.TryParse(value, out number))
+//	{
+//		total += number;
+//	}
+//	else
+//	{
+//		message += value;
+//	}
+//}
+
+//Console.WriteLine($"Message: {message}");
+//Console.WriteLine($"Total: {total}");
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// The Convert class is best for converting the fractional decimal numbers into whole integer numbers
+// Convert.ToInt32() rounds up the way you would expect.
+int result1 = Convert.ToInt32((decimal)value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
